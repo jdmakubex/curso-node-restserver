83920c55ecbbd0c6ee4019f87cd3449987ad12d6
Juan de Dios <jd_marg@msn.com>
2023-10-18 18:20:31 -0600

    Get: Obtener Datos de forma paginada

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,38 +1,59 @@
 const { response, request } = require ('express');
+const bcryptjs = require ('bcryptjs');
 
-const usuariosGet = (req = request, res = response) => {
 
-    const {q, nombre = 'No Name', apikey, page=1, limit} = req.query;
+
+const Usuario = require('../models/usuario');
+
+const usuariosGet = async(req = request, res = response) => {
+
+    //const {q, nombre = 'No Name', apikey, page=1, limit} = req.query;
+    const { limite = 5, desde = 0} = req.query;
+    const usuarios = await Usuario.find()
+        .skip (Number( desde ))
+        .limit(Number(limite));
 
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
+    usuarios
     });
 }
 
-const usuariosPost = (req, res) => {
+const usuariosPost = async(req, res) => {    
 
-    const {nombre,edad} = req.body;
+    const { nombre,correo, password, rol } = req.body;
+    const usuario = new Usuario( { nombre, correo, password, rol } );
+
+    
+
+
+    //Encriptar la constraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt );
+
+    //
+
+    await usuario.save();
 
     res.json({
         msg: 'post API - controlador',
-        nombre,
-        edad
+        usuario
     });
 }
 
-const usuariosPut =  (req, res) => {
+const usuariosPut =  async(req, res = response) => {
 
     const {id} = req.params;
+    const { _id, password,google, correo, ...resto } =req.body;
 
-    res.json({
-        msg: 'put API - controlador',
-        id
-    });
+    //TODO validar contra base de datos
+    if ( password ) {
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt );
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+    res.json( usuario );
 }
 
 const usuariosPatch = (req, res) => {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,16 @@
 
 const { Router } = require ('express');
+const { check } =  require ('express-validator');
+const Role = require ('../models/role');
+
+const { validarCampos } = require ('../middlewares/validar-campos');
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
+
 const { usuariosGet, 
         usuariosPut, usuariosPost, 
         usuariosPatch, 
         usuariosDelete } = require('../controllers/usuarios');
+
 
 const router = Router();
 
@@ -11,9 +18,25 @@ const router = Router();
 
 router.get('/', usuariosGet );
 
-router.post('/', usuariosPost);
+router.post('/', [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password debe contener 6 caracteres al menos').isLength({ min: 6 }),
+        //check('correo', 'El correo no es válido').isEmail(),
+        check('correo').custom(emailExiste),
+        //Aquí validamos el rol, desde elementos de un arreglo
+        //check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+        
+        //Haremos la misma validación pero con un catalogo que está en la BD
+        check('rol').custom( esRoleValido ),
+        validarCampos
+], usuariosPost);
 
-router.put('/:id',usuariosPut);
+router.put('/:id',[
+      check('id','No es un ID válido').isMongoId(),
+      check('id').custom(existeUsuarioPorId),
+      check('rol').custom( esRoleValido ),
+      validarCampos 
+], usuariosPut);
 
 router.patch('/', usuariosPatch);
 
